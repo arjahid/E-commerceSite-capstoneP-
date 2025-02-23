8acbda6e6868e6348d907e774cb2fa9cbe6a9182
arjahid <jhjahidhasanar@gmail.com>
2025-02-23 23:21:03 +0600

    added marquee text and main section

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import NavBar from '../NavBar';
 import Carosel from './Carosel';
+import Marquee from 'react-fast-marquee';
+import LeftAside from '../components/MainContent/LeftAside';
+import RightAside from '../components/MainContent/RightAside';
+import { Outlet } from 'react-router-dom';
 
 const HomeLayouts = () => {
     return (
         <div className='container mx-auto'> 
             <NavBar></NavBar>
             <Carosel></Carosel>
-            <main></main>
+            <Marquee >
+                <h2 className='text-2xl font-bold'>🔥 Mega Sale Live! Up to 50% OFF – Shop Now! 🔥</h2>
+            </Marquee>
+            <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+        <aside className="left col-span-3">
+          <LeftAside></LeftAside>
+        </aside>
+        <section className="col-span-6">Main section
+            <Outlet></Outlet>
+        </section>
+       
+        <aside className="col-span-3"><RightAside></RightAside></aside>
+      </main>
         </div>
     );
 };
