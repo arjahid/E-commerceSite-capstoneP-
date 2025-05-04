31192678b2b7ce21ebe76bd6fdf5b1967456d386
arjahid <jhjahidhasanar@gmail.com>
2025-05-05 03:48:38 +0600

    fix

---
+++ src/components/alldata.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const alldata = () => {
+    return (
+        <div className="p-6 bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300 min-h-screen flex items-center justify-center">
+            <h1 className="text-3xl font-bold text-blue-600 underline decoration-yellow-500">
+                I am from All Data
+            </h1>
+        </div>
+    );
+};
+
+export default alldata;
