c8cce1af9f2e6c63e19b617d0917b0e2e5a115f2
arjahid <jhjahidhasanar@gmail.com>
2025-05-06 23:40:33 +0600

    added admin details

---
+++ src/components/Admin/Admint.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Admint = () => {
+    return (
+        <div>
+            <h2>I am from</h2>
+        </div>
+    );
+};
+
+export default Admint;
---
+++ src/components/Computers/customerDetails/customerDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const customerDetails = () => {
+    return (
+        <div>
+            <h2>hi i am from customer site</h2>
+        </div>
+    );
+};
+
+export default customerDetails;
